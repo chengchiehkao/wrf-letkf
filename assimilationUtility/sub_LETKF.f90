d56051f2b304ea40e847d806e39d3659d70d2e59
Cheng-Chieh Kao <chengchiehkao@gmail.com>
2015-07-16 18:24:52 +0800

    Cleaned up some useless comments in assimilationUtility/sub_LETKF.f90.

--- assimilationUtility/sub_LETKF.f90
+++ assimilationUtility/sub_LETKF.f90
@@ -21,16 +21,10 @@ real(realKind),dimension(n,1) :: yb_mean
 real(realKind),dimension(n,k) :: yb_pert
 real(realKind),dimension(m,m) :: pa
 real(realKind),dimension(k,k) :: pa_tilde
-!real(realKind),dimension(k,k) :: identityMat
 
 integer i  ! loop index
 !================================================
-!print*,shape(xb_mean(:,:)),shape(xb_pert(:,:)),shape(xa_mean(:,:)),shape(xa_pert(:,:)),shape(yo(:,:)),shape(yb(:,:)),shape(R(:,:)),m,n,k
 
-!identityMat(:,:)=0d0
-!do i=1,k
-!    identityMat(i,i)=1d0
-!enddo
 do i=1,n
     yb_mean(i,1)=sum(yb(i,:))/real(k,8)
     yb_pert(i,:)=yb(i,:)-yb_mean(i,1)
@@ -39,8 +33,6 @@ enddo
 pa_tilde = inv( dble(k-1)*identityMat(k) + &
                 matmul( matmul_latterDiag(transpose(yb_pert(:,:)),inv_diagMat(R(:,:),n),k,n),yb_pert(:,:) ) &
                 ,k )
-
-! pa = 1d0/dble(k-1) *matmul( xa_pert(:,:) , transpose(xa_pert(:,:)) )
 
 xa_mean = xb_mean + &
           matmul( &
@@ -55,13 +47,6 @@ xa_mean = xb_mean + &
           ), &
             (yo(:,:)-yb_mean(:,:)) &
           )
-
-!read(*,*)
-!print*,xb_pert(:,:)
-!print*,repeat('=',20)
-!print*,shape(xb_mean(:,:)),shape(xb_pert(:,:)),shape(xa_mean(:,:)),shape(xa_pert(:,:)),shape(yo(:,:)),shape(yb(:,:)),shape(R(:,:)),m,n,k
-!print*,matmul(xb_pert(:,:),pa_tilde(:,:))
-!print*,matmul(xb_pert(1:2,1:2),pa_tilde(1:2,1:2))
 
 xa_pert = matmul( xb_pert(:,:) , matSqrt(dble(k-1)*pa_tilde(:,:),k) )
 
