aa22bf8de9005e21ae915b4578c2b63c58ebad4f
Cheng-Chieh Kao <chengchiehkao@gmail.com>
2015-08-03 16:00:25 +0800

    Changed the implementation of determination of obs%varName from partial name to full name.

--- systemUtility/sub_setSoundingError.f90
+++ systemUtility/sub_setSoundingError.f90
@@ -18,16 +18,16 @@ do io = 1 , sounding%obsNum
 
     if ( sounding%obs(io)%available ) then  ! SHALL AWARE OF PSFC
 
-        !select case ( trim(adjustl(sounding%obs(io)%varName)) )
-        select case (sounding%obs(io)%varName(1:1))
+        select case ( trim(adjustl(sounding%obs(io)%varName)) )
+        !select case (sounding%obs(io)%varName(1:1))
         case ( 'U' )
             sounding%obs(io)%error = 1.d0
         case ( 'V' )
             sounding%obs(io)%error = 1.d0
         case ( 'T' )
             sounding%obs(io)%error = 0.5d0
-        !case ( 'QVAPOR' )
-        case ( 'Q' )
+        case ( 'QVAPOR' )
+        !case ( 'Q' )
             sounding%obs(io)%error = 0.2d-3
         end select
 
